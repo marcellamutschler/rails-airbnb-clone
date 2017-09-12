da94e545a19b6d0335f8ea7df03148a62a909551
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-12 16:24:25 +0200

    facebook done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root to: "venues#index"
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :venues do
     resources :bookings
   end
 
   resources :profile
+
 
 end
 
