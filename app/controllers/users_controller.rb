ac50a5d1e4ae31e34b346cc04ae70d8bc6c4e356
Capucine Cusinberche <capucine.cusinberche@gmail.com>
2017-09-12 18:09:30 +0200

    created Profile Controler and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,12 @@
 class UsersController < ApplicationController
-  def index
-    @tasks = Manager.all
-  end
+
 
   def show
   end
 
-  def new
-    @task = Manager.new
-  end
-
-  def create
-    new_task = Manager.new(task_params)
-    new_task.save
-    redirect_to managers_path
-  end
-
-  def edit
-    @task = Manager.find(params[:id])
-  end
-
-  def update
-    @task.update(task_params)
-    redirect_to manager_path(@task)
-  end
-
-  def destroy
-    @task.destroy!
-    redirect_to managers_path
-  end
 
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :venues do
     resources :bookings
+    # creermoi toutes les routes
   end
 
-  resources :profile
+#  resources :users do
+    resource :profile
+    # ici on parle de 2 méthodes
+    #génére  toutes les routes mais emt profile au singulier
+ # end
 
 end
 
