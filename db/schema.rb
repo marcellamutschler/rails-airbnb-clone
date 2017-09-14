41ec3fe04e411df474ed52b7ddfe54bd42abaf8b
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-14 18:23:21 +0200

    done

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170914140126) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +38,15 @@ ActiveRecord::Schema.define(version: 20170914140126) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -84,6 +92,8 @@ ActiveRecord::Schema.define(version: 20170914140126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
     t.string "photo"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
