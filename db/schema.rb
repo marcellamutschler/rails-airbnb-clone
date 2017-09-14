740115849e361fbd72fda60bd0ba8b125413d90d
Mark Terenz <dasso007@hotmail.com>
2017-09-14 16:10:56 +0200

    fixing bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913130516) do
+ActiveRecord::Schema.define(version: 20170914140126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170913130516) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.integer "status"
+    t.integer "status", default: 2
     t.float "total_price"
     t.bigint "user_id"
     t.bigint "venue_id"
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 20170913130516) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
-  end
-
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -92,8 +83,6 @@ ActiveRecord::Schema.define(version: 20170913130516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.float "latitude"
-    t.float "longitude"
     t.string "photo"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
