27450d463ab3a8d72f905ed6654573b1a87619a8
Mark Terenz <dasso007@hotmail.com>
2017-09-14 11:48:53 +0200

    fixed profile edit bug

--- db/schema.rb
+++ db/schema.rb
@@ -40,15 +40,6 @@ ActiveRecord::Schema.define(version: 20170913130516) do
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
 
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
-  end
-
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -92,8 +83,6 @@ ActiveRecord::Schema.define(version: 20170913130516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.float "latitude"
-    t.float "longitude"
     t.string "photo"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
