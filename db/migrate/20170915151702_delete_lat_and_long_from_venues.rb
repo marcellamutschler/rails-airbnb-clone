e5c8ed1469eab07a9f4ce59ee29074fa5b5b6429
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-15 17:21:12 +0200

    Fixing heroku

--- db/migrate/20170915151702_delete_lat_and_long_from_venues.rb
+++ db/migrate/20170915151702_delete_lat_and_long_from_venues.rb
@@ -1,7 +1,7 @@
 class DeleteLatAndLongFromVenues < ActiveRecord::Migration[5.1]
   def change
-    remove_column :venues,:latitude
-    remove_column :venues,:longitude
+    # remove_column :venues,:latitude
+    # remove_column :venues,:longitude
 
     add_column :venues, :latitude, :float
     add_column :venues, :longitude, :float
