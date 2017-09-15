8456db267a5b3f84280d2b4d89665cb47d587314
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-15 16:50:37 +0200

    better now

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,8 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  geocoded_by :location
+  after_validation :geocode, if: :location_changed?
 
   validates :name, presence: true
   validates :capacity, presence: true
@@ -11,5 +13,7 @@ class Venue < ApplicationRecord
   has_attachment :photo
 
   # mount_uploader :photo, PictureUploader
-
+  def geocoded?
+    self.latitude && self.longitude
+  end
 end
