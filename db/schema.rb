d3c2b28e824e9b9f9c15d0a775cb67f1e2ea8ddd
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-19 18:13:47 +0200

    footer, edit profile

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 20170919135000) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.bigint "conversation_id"
     t.text "content"
@@ -129,12 +138,11 @@ ActiveRecord::Schema.define(version: 20170919135000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "categories", default: [], array: true
-    t.string "amenities", default: [], array: true
     t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.integer "wishlist_id"
+    t.string "categories", default: [], array: true
+    t.string "amenities", default: [], array: true
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
