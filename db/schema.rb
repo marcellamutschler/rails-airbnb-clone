13121463e97010cbe3ebb994922454e3a9f84fd6
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-21 11:02:27 +0200

    fixed bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170920131943) do
 
   # These are extensions that must be enabled in order to support this database
@@ -55,7 +54,6 @@ ActiveRecord::Schema.define(version: 20170920131943) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id"
-<<<<<<< HEAD
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "venue_id"
@@ -76,19 +74,6 @@ ActiveRecord::Schema.define(version: 20170920131943) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
-=======
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "venue_id"
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.integer "owner_id"
-    t.integer "booker_id"
-    t.integer "venue_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
->>>>>>> 6df7f4042daf2f503c6567fa67a8a95aea98e0cb
   end
 
   create_table "messages", force: :cascade do |t|
@@ -156,7 +141,6 @@ ActiveRecord::Schema.define(version: 20170920131943) do
     t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.integer "wishlist_id"
     t.string "categories", default: [], array: true
     t.string "amenities", default: [], array: true
     t.string "city"
