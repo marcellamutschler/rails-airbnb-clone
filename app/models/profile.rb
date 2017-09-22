fec78d5bd765ac52d21675367b33999c8c2c1539
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-22 15:54:14 +0200

    turned off mailer

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   has_attachment :photo
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
 
   private
