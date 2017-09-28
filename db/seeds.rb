eb751bc35724e881720fe14658b36595b198a2e0
Luca Nioi <lucahidekinioi@gmail.com>
2017-09-28 11:55:25 +0200

    fixing convo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,27 @@ RAMDOM_PEOPLE = 'http://lorempixel.com/640/480/people'
 # User.destroy_all
 
 
-# 100.times do
-#   user = User.new(email: Faker::Internet.email, password: '12345678')
-#   user.save
-#   profile = Profile.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-#   profile.user = user
+2.times do
+  user = User.new(email: Faker::Internet.email, password: '12345678')
+  user.save
+  profile = Profile.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  profile.user = user
 
-#   profile.photo_url = RAMDOM_PEOPLE
-#   puts "user: #{user.full_name} created" if profile.save
+  profile.photo_url = RAMDOM_PEOPLE
+  puts "user: #{user.full_name} created" if profile.save
 
-#   20.times do
-#     category_venue = [Venue::CATEGORIES.sample]
-#     amenitiy_venue = [Venue::AMENITIES.sample]
-#     city = CITY_VENUE.sample
-#     venue_name = ADJECTIVE_VENUE.sample + " " + category_venue.first + " " + "Venue"
-#     venue = Venue.new(name: venue_name, categories: category_venue, amenities: amenitiy_venue, city: city, capacity: rand((20)+1)*10, location: city, description: Faker::Lorem.sentence, price: rand(20..200))
+  5.times do
+    category_venue = [Venue::CATEGORIES.sample]
+    amenitiy_venue = [Venue::AMENITIES.sample]
+    city = CITY_VENUE.sample
+    venue_name = ADJECTIVE_VENUE.sample + " " + category_venue.first + " " + "Venue"
+    venue = Venue.new(name: venue_name, categories: category_venue, amenities: amenitiy_venue, city: city, capacity: rand((20)+1)*10, location: city, description: Faker::Lorem.sentence, price: rand(20..200))
 
-#     venue.photo_urls = [RAMDOM_VENUE_IMAGE]
-#     venue.user = user
-#     puts "venue: #{venue.name} created" if venue.save
-#   end
-# end
+    venue.photo_urls = [RAMDOM_VENUE_IMAGE]
+    venue.user = user
+    puts "venue: #{venue.name} created" if venue.save
+  end
+end
 
 # streets = ["carrer d'Aldana", "carrer de Balmas", "carrer del Bruc", "carrer de Buenos Aires", "carrer de Corsega", "carrer de Nicaragua", "placa de la Universitat"]
 
