e6f6076d9ac464e1b93b318f04a26530ebfa65aa
Celia Mutschler <celia.mutschler@web.de>
2017-09-28 12:33:41 +0200

    cards look better

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,8 @@ class PagesController < ApplicationController
     # the name of the method has to do with our routes
     # rails.routes --> show#home
     @cities = Venue.where.not(city: nil).to_a.map{|b| b.city}.uniq.sort
-    #AR method to guet cities frim venues that have cities, then
+
+        #AR method to guet cities frim venues that have cities, then
     # it returns us an array of cities
     @categories = ['Wedding', 'Party', 'Baptism', 'Workshop', 'Training', 'Social Event', 'Desk Rental', 'Conference', 'Launch Event', 'Business Meeting', 'Birthday', 'Photo Shoot', 'Film Shoot', 'Concert', 'Get-Together'].sort
     #@city = params["city"]
